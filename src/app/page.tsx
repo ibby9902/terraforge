bd0ff9e826805bea2a95649ac024f6f865b8a6a5
Ibby <ibby364@outlook.com>
2023-11-30 00:10:50 +0000

    fix item card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import { getServerAuthSession } from "@/server/auth";
 import { buttonVariants } from "@/components/ui/button";
 
 export function generateMetadata() {
@@ -8,7 +9,9 @@ export function generateMetadata() {
   };
 }
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await getServerAuthSession();
+
   return (
     <main className="w-full h-full">
       <div className="flex flex-col w-full justify-center items-center h-96 gap-6 pt-48">
@@ -18,7 +21,7 @@ export default function HomePage() {
         <p className="text-center text-xl sm:text-2xl md:text-3xl sm:px-32">Discover, play, and share Terraria mods through our open-source platform built for the community.</p>
         <div className="flex gap-4">
           <Link href="/mods" className={buttonVariants({ variant: "default" })}>Discover mods</Link>
-          <Link href="/sign-up" className={buttonVariants({ variant: "outline" })}>Sign up</Link>
+          {session ? null : <Link href="/sign-up" className={buttonVariants({ variant: "outline" })}>Sign up</Link>}
         </div>
       </div>
     </main>
--- src/components/project-item-card.tsx
+++ src/components/project-item-card.tsx
@@ -15,22 +15,20 @@ interface Props {
 
 const ProjectItemCard = ({ id, slug, name, authorName, icon, summary, numDownloads } : Props) => {
   return (
-    <div className='bg-accent rounded-xl grid grid-cols-2 sm:grid-cols-3 p-6 grid-rows-4 sm:grid-rows-3 gap-4'>
-      <ModIcon imageUrl={icon} className='w-32 row-span-2 sm:row-span-3 col-end-1'/>
-      <div className='flex items-end gap-2 col-span-2 flex-wrap'>
+    <div className='bg-accent rounded-xl p-4 grid md:grid-cols-2 gap-4'>
+      <Link href={`/mod/${slug}`} className='md:row-span-3 col-end-1 w-28'>
+        <ModIcon imageUrl={icon} />
+      </Link>
+      <div className='flex items-end gap-2 md:col-span-3 flex-wrap'>
         <Link href={`/mod/${slug}`} className='font-bold text-2xl'>{name}</Link>
         <span>by</span>
         <Link href={`/user/${authorName}`} className='underline hover:text-gray-500'>{authorName}</Link>
       </div>
-      <div></div>
-      <div className='col-span-3'><p className=''>{summary ?? <span className='italic'>No summary</span>}</p></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div className='hidden md:block'></div>
-      <div className='flex items-center gap-2'><Download size={16}/><span className='font-bold'>{numDownloads}</span> downloads</div>
-      <div></div>
-      <div className='flex justify-end items-center gap-2'><RefreshCcw size={16}/>Updated 10 years ago</div>
+      <div className='md:col-span-3'>
+        <p className=''>{summary ?? <span className='italic'>No summary</span>}</p>
+      </div>
+      <div className='flex items-center gap-2 md:col-span-2'><Download size={16}/><span className='font-bold'>{numDownloads}</span> downloads</div>
+      <div className='flex items-center gap-2 md:col-start-3'><RefreshCcw size={16}/>Updated 10 years ago</div>
     </div>
   );
 };
