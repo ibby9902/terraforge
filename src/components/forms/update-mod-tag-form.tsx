2233cff7fa7e44fd5877372a992b3abed81ae7b1
Ibby <ibby364@outlook.com>
2023-12-18 22:40:10 +0000

    add form and checkbox

--- src/components/forms/update-mod-tag-form.tsx
+++ src/components/forms/update-mod-tag-form.tsx
@@ -1,10 +1,20 @@
 "use client";
 import React, { useState } from 'react';
+import type z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import { useToast } from "@/components/ui/use-toast";
 import type { TAG_TYPE } from '@/lib/validation/project';
-import { Toggle } from '@/components/ui/toggle';
-import { cn } from '@/lib/utils';
+import {
+  Form,
+  FormField,
+  FormItem,
+} from '@/components/ui/form';
+import { Label } from '@/components/ui/label';
+import { Checkbox } from '@/components/ui/checkbox';
+import { updateModTagSchema } from '@/lib/validation/project';
+import type { CheckedState } from '@radix-ui/react-checkbox';
 
 interface Props {
   modId: string;
@@ -18,15 +28,16 @@ interface Props {
 const UpdateModTagForm = ({ modId, isActive, tagType, text, icon, className }: Props) => {
   const [toggled, setToggled] = useState(isActive);
   const { toast } = useToast();
-
-  const onClick = async (value: boolean) => {
-    setToggled(value);
-
-    const values = {
+  const form = useForm<z.infer<typeof updateModTagSchema>>({
+    resolver: zodResolver(updateModTagSchema),
+    defaultValues: {
       modId,
       tagName: tagType,
-      action: value ? "add" : "delete",
+      action: isActive ? "delete" : "add",
     }
+  });
+
+  const onSubmit = async (values: z.infer<typeof updateModTagSchema>) => {
 
     try {
       const response = await fetch("/api/mod/update-tag", {
@@ -61,10 +72,32 @@ const UpdateModTagForm = ({ modId, isActive, tagType, text, icon, className }: P
     }
   }
 
+  const onCheckedChange = (checked: CheckedState) => {
+    const value = Boolean(checked);
+    setToggled(value);
+
+    value  ? form.setValue("action", "add") : form.setValue("action", "delete");
+
+    void form.handleSubmit(onSubmit)();
+  }
+
   return (
-    <Toggle pressed={toggled} onPressedChange={onClick} className={cn("w-20 rounded-full data-[state=off]:bg-blue-500 text-xs")}>
-      {text}
-    </Toggle>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col gap-2' >
+        <FormField
+          control={form.control}
+          name="action"
+          render={({ field }) => (
+            <FormItem>
+              <div className='flex gap-2'>
+                <Checkbox {...field} checked={toggled} onCheckedChange={onCheckedChange} />
+                <Label>{text}</Label>
+              </div>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 }
 
